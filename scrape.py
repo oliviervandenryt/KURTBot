910f8baef2e253e4efa8488ccae6e3cbdc33296a
oliviervandenryt <olivier.vandenryt@student.kuleuven.be>
2020-12-28 23:03:58 +0100

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from functions import *
+from datetime import date, timedelta
+
+
+''' PARAMETERS '''
+username = "r0******"
+password = "********"
+times = [["08:00:00", "12:00:00"],  # tijdsloten van 4u
+         ["12:00:00", "16:00:00"],
+         ["16:00:00", "20:00:00"]
+         ]
+seat = str(106602) # zie .csv bestanden
+date = str(date.today() + timedelta(days=2)) # bv. '2020-12-30'
+subject = "Study"
+
+
+'''CODE'''
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.switch_to.window(driver.current_window_handle)
+login(driver, username, password)
+
+scrape(driver, 100000, 100100, 'bib')
