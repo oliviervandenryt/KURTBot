43da74c1724a1b97e2f991b342a7a4392f6572ff
oliviervandenryt <olivier.vandenryt@student.kuleuven.be>
2020-12-30 21:40:55 +0100

    general sleep parameter

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ from datetime import date, timedelta
 username = "r0******"
 password = "********"
 times = [["08:00:00", "12:00:00"],  # tijdsloten van 4u
-         ["12:00:00", "16:00:00"],
-         ["16:00:00", "20:00:00"]
+         ["13:00:00", "17:00:00"],
+         ["18:00:00", "22:00:00"]
          ]
-seat = str(106602) # zie .csv bestanden
-date = str(date.today() + timedelta(days=2)) # bv. '2020-12-30'
+seat = str(106602)  # zie .csv bestanden
+date = str(date.today() + timedelta(days=2))  # bv. '2020-12-30'
 subject = "Study"
+default_sleep = 3.0  # bij problemen of traag internet verhogen
 
 
 '''CODE'''
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.switch_to.window(driver.current_window_handle)
-login(driver, username, password)
+login(driver, username, password, sleep_time=default_sleep)
 
 for time in times:
-    reserve(driver, date, time, seat, subject)
+    reserve(driver, date, time, seat, subject, sleep_time=default_sleep)
 driver.close()
