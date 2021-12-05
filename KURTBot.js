071d4841ffc884c7eb227379fd286560c74071ef
Olivier Vandenryt <72090160+oliviervandenryt@users.noreply.github.com>
2021-12-05 12:31:18 +0100

    Update KURTBot.js
    Implementation of SessionID using WebProxy service

--- KURTBot.js
+++ KURTBot.js
@@ -8,13 +8,13 @@ let times = [
     ["08:00:00", "10:00:00"]
 ];
 // -Seat ID-
-let sid = 'XXXXXX';
+let sid = '012345';
 // -User ID-
-let user = 'rXXXXXXX';
+let user = 'rxxxxxxx';
 // -Days in advance (def. 4)-
 let days_in_advance = 4;
 // -Subject-
-let subject = 'Study'
+let subject = 'Study';
 // --CODE, DO NOT TOUCH--
 
 // Create date function & object
@@ -27,7 +27,11 @@ var date = new Date();
 var usedDate = date.addDays(days_in_advance);
 
 // Encode subject
-let subject_base = btoa(subject)
+let subject_base = btoa(subject);
+// Get session-id
+var newSessionID;
+let sessionFetch = await fetch(`https://www-sso.groupware.kuleuven.be/sites/KURT/_vti_bin/WebProxy/WebProxy.svc/CallService?webserviceUrl=https://wsrts.ghum.kuleuven.be/service1.asmx/GetSessionID2?CurrentUID=${user}`);
+newSessionID = await sessionFetch.json();
 // Reserve function
 var reserve = async _ => {
     // Loops over each timeslot
@@ -49,25 +53,21 @@ var reserve = async _ => {
         var stringDate2 = usedDate2.getFullYear() + '-' +
             String(usedDate2.getMonth() + 1).padStart(2, '0') + '-' +
             String(usedDate2.getDate()).padStart(2, '0');
-        let url = `https://www-sso.groupware.kuleuven.be/sites/KURT/_vti_bin/KURTService/KURTService.svc/CallWebservice?webserviceUrl=/CreateReservation4?CurrentUID=${user}%26SelectedResourceID=${sid}%26SelectedBeginDatestring=${stringDate}%20${starttime}%26SelectedEndDatestring=${stringDate2}%20${endtime}%26Subject=${subject_base}%26Body=%26OtherUsers=%26Answer=%26ImpersonatedUID=%26SessionID=`;
+        let url = `https://www-sso.groupware.kuleuven.be/sites/KURT/_vti_bin/KURTService/KURTService.svc/CallWebservice?webserviceUrl=/CreateReservation4?CurrentUID=${user}%26SelectedResourceID=${sid}%26SelectedBeginDatestring=${stringDate}%20${starttime}%26SelectedEndDatestring=${stringDate2}%20${endtime}%26Subject=${subject_base}%26Body=%26OtherUsers=%26Answer=%26ImpersonatedUID=%26SessionID=${newSessionID}`;
         // Sending request to server
         (async () => {
             const res = await fetch(url);
-            const data = await res.json();
+            const data = await res.text();
             // Retries when response is too short
+            console.log(data);
             if (data.length < 5) {
                 alert(starttime + ' - ' + endtime + ': FAILED, trying again\n');
                 const res = await fetch(url);
                 const data = await res.json();
-                alert(starttime + ' - ' + endtime + ':\n' + data.slice(54, -9) + '\n');
-                if (data.length < 5) {
-                    alert(starttime + ' - ' + endtime + ': FAILED again, try other seat\n');
-                    alert(starttime + ' - ' + endtime + ':\n' + data.slice(54, -9) + '\n');
-                } else {
-                    alert(starttime + ' - ' + endtime + ':\n' + data.slice(54, -9) + '\n');
+                console.log(data);
                 }
-            } else {
-                alert(starttime + ' - ' + endtime + ':\n' + data.slice(54, -9) + '\n');
+            else {
+                console.log(starttime + ' - ' + endtime + ':\n' + data.slice(54, -9) + '\n');
             }
         })();
     }
